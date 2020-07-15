ae12b7a64242ce22eb959e52654757190a835cc3
agafonovilya <agafonov.i.m@gmail.com>
2020-07-15 17:33:10 +0300

    Homework 6 implemented

--- app/src/main/java/ru/geekbrains/myweatherapp/fragments/FragmentMainScreen.java
+++ app/src/main/java/ru/geekbrains/myweatherapp/fragments/FragmentMainScreen.java
@@ -8,10 +8,16 @@ import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import ru.geekbrains.myweatherapp.MainActivityByFragment;
 import ru.geekbrains.myweatherapp.R;
+import ru.geekbrains.myweatherapp.weekForecastByRecyclerView.SourceOfWeekForecastCard;
+import ru.geekbrains.myweatherapp.weekForecastByRecyclerView.WeekForecastAdapter;
 
 public class FragmentMainScreen extends Fragment {
     @Override
@@ -22,14 +28,14 @@ public class FragmentMainScreen extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ImageButton startSettingsFragment = view.findViewById(R.id.imageButton);
+        ImageButton startSettingsFragment = (ImageButton) view.findViewById(R.id.imageButton);
         startSettingsFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((MainActivityByFragment)getActivity()).startFragment(2);
             }
         });
-        ImageButton startCitySelectionFragment = view.findViewById(R.id.imageButton3);
+        ImageButton startCitySelectionFragment = (ImageButton) view.findViewById(R.id.imageButton3);
         startCitySelectionFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,5 +43,27 @@ public class FragmentMainScreen extends Fragment {
             }
         });
 
+        SourceOfWeekForecastCard sourceData = new SourceOfWeekForecastCard(getResources());
+        initRecyclerView(sourceData.build(), view);
+
     }
+
+    private void initRecyclerView(SourceOfWeekForecastCard sourceData, View view){
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.weekForecast);
+
+        // Эта установка служит для повышения производительности системы
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        WeekForecastAdapter adapter = new WeekForecastAdapter(sourceData);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(recyclerView.getContext(),  layoutManager.getOrientation());
+        itemDecoration.setDrawable(ContextCompat.getDrawable(getContext(), R.drawable.separator));
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
 }
